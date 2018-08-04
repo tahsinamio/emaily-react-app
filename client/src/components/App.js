87096adb3dce60104e3c5d4504284ca767c7fddb
Tahsin Wasit Amio <tahsinamio@gmail.com>
2018-08-04 19:31:29 +0600

    Setup client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const Header = () => <h2>Header</h2>;
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+import Header from './Header';
+import Dashboard from './Dashboard';
+import SurveyNew from './SurveyNew';
+import Landing from './Landing';
 
-const App = () => {
-	return (
-		<BrowserRouter>
-			<div>
-				<Header />
-				<Route exact path="/" component={Landing} />
-				<Route exact path="/surveys" component={Dashboard} />
-				<Route path="/surveys/new" component={SurveyNew} />
+class App extends React.Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
 			</div>
-		</BrowserRouter>
-	);
-};
+		);
+	}
+}
 
-export default App;
+export default connect(
+	null,
+	actions
+)(App);
---
+++ client/src/components/SurveyNew.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SurveyNew extends React.Component {
+	render() {
+		return <div>SurveyNew</div>;
+	}
+}
+
+export default SurveyNew;
