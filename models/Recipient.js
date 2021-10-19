d8685a4d3ab4ca79e2e7f6aad936cb3cb2a4f7d7
Tahsin Wasit Amio <tahsinamio@gmail.com>
2021-10-18 18:34:21 -0700

    added missing files

---
+++ models/Recipient.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const RecipientSchema = new mongoose.Schema({
+	email: String,
+	responded: { type: Boolean, default: false }
+});
+
+module.exports = RecipientSchema;
---
+++ models/Surveys.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const RecipientSchema = require('./Recipient');
+
+const surveySchema = new mongoose.Schema({
+	title: String,
+	subject: String,
+	body: String,
+	recipients: [RecipientSchema],
+	yes: { type: Number, default: 0 },
+	no: { type: Number, default: 0 },
+	_user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	dateSent: Date,
+	lastResponded: Date
+});
+
+mongoose.model('surveys', surveySchema);
